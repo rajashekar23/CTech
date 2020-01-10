e05031efc6bdab6d73950102c12ea3bfe1bfb3a1
sairam <sairambitra7@gmail.com>
2020-01-10 10:55:28 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProcessComponent } from './components/process/process.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeBannerComponent,
+    AboutusComponent,
+    ServicesComponent,
+    ProcessComponent,
+    ContactusComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
